85eb599b51bcc1329ed626423ee0f65c952ed553
Gadir <donatkarta@gmail.com>
2024-12-15 22:41:12 +0400

    Thats my homework

---
+++ homelesson.py
@@ -0,0 +1,6 @@
+opt1 = input("Write your first opinion with numbers : ")
+opt2 = input("Write your second opinion with numbers : ")
+
+result1 = opt1 + opt2
+result2 = int(result1) * 2
+print("Adding string: ", result2)
